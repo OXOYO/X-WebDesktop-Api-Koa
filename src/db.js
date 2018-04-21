6c5b99de2ec467012884bb668c1a5b6d3d415ae8
OXOYO <zmn2007.hi@163.com>
2018-04-21 13:44:37 +0800

    解决NODE_ENV设置不正确的BUG。

--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ import { DB as dbConfig } from './config'
 
 // 判断环境
 let dbType = process.env.NODE_ENV === 'development' ? 'localhost' : 'heroku'
-console.log('dbType', dbType)
+console.log('dbType', dbType, 'NODE_ENV', process.env.NODE_ENV)
 
 const db = new Sequelize(
   dbConfig[dbType].database,
